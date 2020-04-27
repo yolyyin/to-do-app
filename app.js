36a9e4a999b0a3e989a0bf1214bd0ed3a09b8f5e
Tingyun Yin <tingyun_yin@example.com>
2020-04-27 15:23:36 -0400

    assignment completed

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function onReady(){
+
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit',event => {
+
+  addToDoForm.addEventListener('submit',function(event) {
     event.preventDefault();
+
 
     //get the text
     let title = newToDoText.value;
@@ -16,9 +19,21 @@ function onReady(){
     let checkbox =document.createElement('input');
     checkbox.type = "checkbox";
 
+
+
+    let dltbutton=document.createElement('button');
+    dltbutton.textContent = 'delete';
+
+    dltbutton.addEventListener('click',function(event){
+      toDoList.removeChild(this.parentElement);
+    });
+
+
+
     //set the newli text and append checkbox
     newLi.textContent =title;
     newLi.appendChild(checkbox);
+    newLi.appendChild(dltbutton);
 
     //append newli to to-do toDoList
     toDoList.appendChild(newLi);
@@ -27,6 +42,9 @@ function onReady(){
     newToDoText.value ="";
 
   });
+  //delete all checked when press dltbutton
+
+
 }
 
 window.onload=function(){
